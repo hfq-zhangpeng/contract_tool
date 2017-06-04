cafce5e842be56c6f72fd07f947a35c46ec85586
hfq-zhangpeng <zhangpeng@huizhaofang.com>
2017-06-04 13:31:34 +0800

    add : 创建项目

---
+++ config.js
@@ -0,0 +1,142 @@
+var _meta = `<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta http-equiv="Cache-Control" content="no-siteapp">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <meta name="renderer" content="webkit|ie-comp|ie-stand">
+    <meta content="telephone=no" name="format-detection" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />`;
+
+var _phoneStyle = `<style>
+    *,
+    *:before,
+    *:after {
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    }
+    
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    dl,
+    dt,
+    dd,
+    ul,
+    ol,
+    li,
+    img,
+    pre,
+    form,
+    button,
+    textarea,
+    th,
+    td {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        outline: 0;
+    }
+    
+    html,
+    body {
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        vertical-align: baseline;
+    }
+    
+    body,
+    button,
+    input,
+    select,
+    textarea {
+        color: #333;
+        font: 14px/1.5 "Helvetica Neue", Helvetica, "Hiragino Sans GB", "Segoe UI", "Microsoft Yahei", Tahoma, Arial, STHeiti, sans-serif;
+    }
+    
+    h1 {
+        font-size: 2rem;
+    }
+    
+    h2 {
+        font-size: 1.5rem;
+    }
+    
+    h3,
+    h4,
+    h5,
+    h6 {
+        font-size: 1rem;
+    }
+    
+    a {
+        background-color: transparent;
+        text-decoration: none;
+        outline: 0;
+    }
+    
+    img {
+        border: 0;
+        -ms-interpolation-mode: bicubic;
+    }
+    
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+    
+    input,
+    textarea,
+    select {
+        outline: 0;
+    }
+    
+    textarea {
+        resize: none;
+    }
+    
+    ul,
+    ol {
+        list-style: none;
+    }
+    </style>`;
+
+var _style = `<style>
+                html,
+                body {
+                    width: 100%;
+                    height: 100%;
+                    margin: 0;
+                    padding: 10px 5px;
+                    line-height: 26px;
+                    font-size: 16px;
+                }
+
+                .line {
+                    min-width: 80px;
+                    text-align:center;
+                    border-bottom:#000 solid 1px;
+                    display: inline-block;
+                    line-height:100%;
+                }
+
+                table {
+                    table-layout: fixed;
+                    width: 100%;
+                    font-size: 14px;
+                }
+                
+                table td {
+                    border: #dfdfdf solid 1px;
+                    text-align: center;
+                }
+            </style>`;
---
+++ js/contract.js
@@ -0,0 +1,71 @@
+var ue = UE.getEditor('editor');
+
+
+function getContent() {
+    var content = ue.getContent();
+
+    content = replaceContent(content);
+
+    console.log(createNewDom(content));
+    // console.log(content);
+}
+
+/**
+ * 对content进行处理，消除实体名称、字体样式
+ * @param  {[type]} content [description]
+ * @return {[type]}         [description]
+ */
+function replaceContent(content) {
+    content = content.replace(/&quot;/g, "'");
+
+    content = content.replace(/font-family[^;]*?"/g,'"');
+
+    content = content.replace(/font-family.*?;/g, '');
+
+    content = content.replace(/font-size[^;]*?"/g,'"');
+
+    content = content.replace(/font-size.*?;/g, '');
+
+    content = content.replace(/span.*?underline;"/g, 'span class="line"');
+
+    content = content.replace(/-webkit-tap-highlight-color[^;]*?"/g, '"');
+
+    content = content.replace(/-webkit-tap-highlight-color.*?;/g, '');
+
+    content = content.replace(/(&nbsp;){2,}/g, '');
+
+    content = content.replace(/(&nbsp; ){2,}/g, '');
+
+    content = content.replace(/color[^;]*?51\)"/g, '"');
+
+    content = content.replace(/color.*?51\);/g, '');
+
+    content = content.replace(/style=" "/g, '');
+
+    content = content.replace(/width=".*?"/g,'');
+
+    content = content.replace(/height=".*?"/g,'');
+
+    return content;
+}
+
+/**
+ * 创建新的页面dom文本
+ * @param  {[type]} content [description]
+ * @return {[type]}         [description]
+ */
+function createNewDom(content) {
+    var _html = [];
+    _html.push('<!DOCTYPE html>');
+    _html.push('<html>');
+    _html.push('<title></title>');
+    _html.push(_meta);
+    _html.push(_phoneStyle);
+    _html.push(_style);
+    _html.push('<body>');
+    _html.push(content);
+    _html.push('</body>');
+    _html.push('</html>');
+
+    return _html.join('');
+}
